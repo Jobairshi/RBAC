384cdddae6888a5eab3d0ac93c1381bd11a32f72
jobair <jesanuddin12@gmail.com>
2024-07-05 19:03:26 +0600

    swagger added

--- jjproject/src/main.ts
+++ jjproject/src/main.ts
@@ -2,11 +2,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'; // Import the dotenv module
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, options); // Create the Swagger document
+  SwaggerModule.setup('api', app, document); // Set up the Swagger module
+
   await app.listen(4000);
 }
 bootstrap();
